6b1643c9606fd7a07a2da7ba71abd541fa416e0a
jameshsu1125 <jameshsu1125@gmail.com>
2026-03-02 22:11:27 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -8,14 +8,20 @@ const Container = memo(({ children, className }: { className?: string } & IReact
   return (
     <Div100vh className={twMerge('Container w-full', className)}>
       <div className='absolute flex h-full w-full flex-col'>
-        <div className='bg-primary h-44 w-full' />
-        <div className='flex w-full flex-1 flex-col bg-white'>
+        <div className='bg-primary xs:h-32 h-24 w-full sm:h-44 md:h-60' />
+        <div className='texture flex w-full flex-1 flex-col bg-white'>
           <div className='body' />
           <div className='footer' />
         </div>
       </div>
       <div className='relative flex h-full w-full justify-center'>
-        <div className='w-full max-w-3xl'>{children}</div>
+        <div className='flex w-full max-w-3xl flex-col items-center justify-start p-2 md:p-4'>
+          <div className='flex w-full flex-row items-center justify-start'>
+            <div className='logo' />
+            <div className='options'>{/** options */}</div>
+          </div>
+          <div className='w-full'>{children}</div>
+        </div>
       </div>
     </Div100vh>
   );
