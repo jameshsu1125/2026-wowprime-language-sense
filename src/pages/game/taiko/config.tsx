2bdf90d363582b57273296f31a8fc68fd6a97c59
jameshsu1125 <jameshsu1125@gmail.com>
2026-03-24 13:13:59 +0800

    feat: 更新 Taiko 設定以調整遊戲難度級別至 75

--- src/pages/game/taiko/config.tsx
+++ src/pages/game/taiko/config.tsx
@@ -16,7 +16,7 @@ export const TaikoTolerance = 10; // 容錯率
 const params = new URLSearchParams(window.location.search);
 const speed = parseInt(params.get('speed') || '4000') || 4000;
 const min = parseInt(params.get('min') || '280') || 280;
-const level = parseInt(params.get('level') || '50') || 50;
+const level = parseInt(params.get('level') || '75') || 75;
 const start = parseInt(params.get('start') || '1000') || 1000;
 
 export const TaikoNoteSpeed = speed;
