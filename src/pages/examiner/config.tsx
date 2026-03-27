90bcf3999dadffc862bcaa466a8f5789b02cd276
jameshsu1125 <jameshsu1125@gmail.com>
2026-03-27 21:27:11 +0800

    feat: 更新字幕設置以改善顯示延遲，並替換視頻封面文件

--- src/pages/examiner/config.tsx
+++ src/pages/examiner/config.tsx
@@ -4,21 +4,17 @@ export const subtitleSetting = [
     index: 1,
     data: [
       {
-        text: '人人參加有獎、週週公布排名，分數愈高獎愈大！',
+        text: '各位考生好，我是你們的主考官，沈春華。',
         delay: 0,
       },
       {
-        text: '各位考生好，我是你們的主考官，沈春華。',
-        delay: 2200,
-      },
-      {
         text: '歡迎參加《全民一起好好吃語感大會考》',
-        delay: 200,
+        delay: 100,
       },
     ],
   },
   {
-    startTime: 12.1,
+    startTime: 6.3,
     index: 2,
     data: [
       {
@@ -27,15 +23,15 @@ export const subtitleSetting = [
       },
       {
         text: '在週排名公布前，可持續重新挑戰測驗，力拚高分！',
-        delay: 2000,
+        delay: 1400,
       },
       {
         text: '最高可獨得17,000點瘋點數！',
-        delay: 2800,
+        delay: 2000,
       },
       {
         text: '準備好了嗎？祝各位考生應試順利！',
-        delay: 1000,
+        delay: 500,
       },
     ],
   },
