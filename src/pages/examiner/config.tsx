627d23d47e13cf3ef031189a44fb946b59f42db2
jameshsu1125 <jameshsu1125@gmail.com>
2026-03-31 11:40:13 +0800

    feat: 更新字幕設定，調整延遲時間為0，並修改樣式以增強可讀性

--- src/pages/examiner/config.tsx
+++ src/pages/examiner/config.tsx
@@ -7,31 +7,55 @@ export const subtitleSetting = [
         text: '各位考生好，我是你們的主考官，沈春華。',
         delay: 0,
       },
+    ],
+  },
+  {
+    startTime: 2.6,
+    index: 2,
+    data: [
       {
         text: '歡迎參加《全民一起好好吃語感大會考》',
-        delay: 100,
+        delay: 0,
       },
     ],
   },
   {
-    startTime: 6.3,
-    index: 2,
+    startTime: 6.25,
+    index: 3,
     data: [
       {
         text: '本次測驗一共有三大題，音調、語境與最後挑戰題。',
         delay: 0,
       },
+    ],
+  },
+  {
+    startTime: 10.954,
+    index: 4,
+    data: [
       {
         text: '在週排名公布前，可持續重新挑戰測驗，力拚高分！',
-        delay: 1400,
+        delay: 0,
       },
+    ],
+  },
+  {
+    startTime: 15.41,
+    index: 5,
+    data: [
       {
         text: '最高可獨得17,000點瘋點數！',
-        delay: 2000,
+        delay: 0,
       },
+    ],
+  },
+  {
+    startTime: 18.41,
+    index: 6,
+    data: [
       {
         text: '準備好了嗎？祝各位考生應試順利！',
-        delay: 500,
+        delay: 0,
       },
     ],
   },
