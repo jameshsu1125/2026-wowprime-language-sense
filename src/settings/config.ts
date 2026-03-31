84097548d129ac2b522f78fe9c716795b31299e6
jameshsu1125 <jameshsu1125@gmail.com>
2026-03-31 16:11:02 +0800

    fix(table): increase border-bottom width for table rows
    chore(config): set IS_TEST to true for testing purposes

--- src/settings/config.ts
+++ src/settings/config.ts
@@ -17,7 +17,7 @@ export const SETTING = {
   checkAnswerReactDuration: 2000,
 };
 
-export const IS_TEST = false;
+export const IS_TEST = true;
 
 export const SECTION_DURATION = 24 * 60 * 60 * 1000; // 1 day
 
